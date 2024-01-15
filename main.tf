b034691b0d10511ae185c36f8c602ad6873e5abb
Your Name <your.email@example.com>
2024-01-14 19:54:32 -0600

    "my_first_terraform_code"

---
+++ main.tf
@@ -0,0 +1,34 @@
+
+  
+
+
+
+# Configuration options
+
+
+resource "aws_iam_user" "devops" {
+  name = "devops"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_access_key" "lb" {
+  user = aws_iam_user.devops.name
+}
+
+data "aws_iam_policy_document" "lb_ro" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "lb_ro" {
+  name   = "test"
+  user   = aws_iam_user.devops.name
+  policy = data.aws_iam_policy_document.lb_ro.json
+}
---
+++ mainec2.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.32.1"
+    }
+  }
+}
+
+provider "aws" {
+    region = "eu-west-1" 
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0905a3c97561e0b69"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "quame"
+  }
+}
